55f8893290ce0be80fc494489e0c15b25da78464
Maksim Sheko <sakusan@mail.ru>
2022-11-07 19:23:24 +0300

    hw4 complete

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -17,12 +17,8 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
-    const finalClassName = s.button
-        // + (disabled
-        //         ? ...
-        //         : xType === 'red'
-        //             ? ...
-        + (className ? ' ' + className : '') // задачка на смешивание классов
+
+    const finalClassName = s.button + (disabled ? ` ${s.disabled}` : xType === 'secondary' ? ` ${s.secondary}` : xType === 'red' ? ` ${s.red}` : ` ${s.default}` )  // задачка на смешивание классов
 
     return (
         <button
--- src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -5,6 +5,7 @@ import React, {
 } from 'react'
 import s from './SuperCheckbox.module.css'
 
+
 // тип пропсов обычного инпута
 type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>,
     HTMLInputElement>
@@ -28,6 +29,8 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
         // задачка на написание онченджа
+        onChangeChecked && onChangeChecked(e.currentTarget.checked)
+        onChange && onChange(e)
 
     }
 
