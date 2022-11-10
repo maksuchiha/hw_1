ddc8999fa0684566789332d2bdceb7cdc9a1fcf0
Maksim Sheko <sakusan@mail.ru>
2022-11-11 02:37:48 +0300

    refactor sidebar

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -31,7 +31,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        className={( navData: any ) => navData.isActive ? s.active : ""}
+                        className={( navData: { isActive: boolean; isPending: boolean; } ) => navData.isActive ? s.active : ""}
                     >
                         Pre-junior
                     </NavLink>
@@ -39,7 +39,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        className={( navData: any ) => navData.isActive ? s.active : ""}
+                        className={( navData: { isActive: boolean; isPending: boolean; } ) => navData.isActive ? s.active : ""}
                     >
                         Junior
                     </NavLink>
@@ -47,7 +47,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        className={( navData: any ) => navData.isActive ? s.active : ""}
+                        className={( navData: { isActive: boolean; isPending: boolean; } ) => navData.isActive ? s.active : ""}
                     >
                         Junior Plus
                     </NavLink>
