18449e5951f3f99da6b562b9538fbf58f8f08a56
Maksim Sheko <sakusan@mail.ru>
2023-01-10 02:32:53 +0300

    hw9 complete

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -5,6 +5,7 @@ import HW5 from "../s2-homeworks/hw05/HW5";
 function App() {
     return (
         <div className={s.App}>
+
             {/*<HW1/>*/}
             {/*<HW2/>*/}
             {/*<HW3/>*/}
